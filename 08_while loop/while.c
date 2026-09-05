2fcdcc743f01418bee84764162cb9ab6877a8614
Shekinah Geneema <shekinahgeneema55@gmail.com>
2026-09-05 12:06:04 +0530

    Implement factorial calculation with user input
    This program calculates the factorial of a given positive number entered by the user in a loop until a negative number is input.

---
+++ 08_while loop/while.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int input,num=5,i=1;
+        while(1){
+        printf("\n Enter your number %d:",i);
+        scanf("%d",&input);
+        if(input>0){
+        int product=1;
+        int j=1;
+        while(j<=input){
+            product*=j;
+            j++;}
+         printf("\n The factorial of a given %d number = %d \n",input,product);
+         i++;
+        }
+        else{
+            printf("you have entered the negative value");
+            break;
+        }
+        }
+
+    return 0;
+}
