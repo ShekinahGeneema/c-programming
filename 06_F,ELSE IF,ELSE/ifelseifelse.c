fa8bdfe5aaaca8ec5ee6fd621e64e6c70c9707ff
Shekinah Geneema <shekinahgeneema55@gmail.com>
2026-09-05 11:11:54 +0530

    Add if-else structure to compare two integers

---
+++ 06_F,ELSE IF,ELSE/ifelseifelse.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+
+    if (a > b)
+    {
+        printf("a is greater than b");
+    }
+    else if (a < b)
+    {
+        printf("a is smaller than b");
+    }
+    else
+    {
+        printf("a and b are equal");
+    }
+
+    return 0;
+}
